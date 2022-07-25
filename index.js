8ca90a9f01a7d085c957447d44d515f17f228475
Hai Nam <hainam@kaist.ac.kr>
2022-07-26 08:45:59 +0900

    update load data, compile and fit model

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-import * as tf from '@tensorflow/tfjs'
+import * as tf from '@tensorflow/tfjs';
+import { MnistData } from './data'
 import * as ui from './ui'
+
+
+let data
+
+async function load(){
+  const data = new MnistData()
+  await data.load()
+}
 
 function createConvModel() {
   //TODO: define model here
@@ -7,8 +16,35 @@ function createConvModel() {
 
 async function train(model, onIteration) {
   //TODO: define model compile
+  model.compile({
+    optimize: 'rmsprop', 
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy']
+  })
 
   //TODO: model.fit
+  const trainData = data.getTrainData()
+  const testData = data.getTestData()
+
+  await model.fit(trainData.xs, trainData.labels, {
+    batchSize, 
+    validationSplit,
+    epochs: trainEpochs,
+    callbacks : {
+      onBatchEnd: async (batch, logs) => {
+        //TODO: print status message and plot graph (loss, accucuracy) ? when batch end
+        
+        await tf.nextFrame()
+      },
+      
+      onEpochEnd: async (epoch, logs) => {
+        //TODO: plot graphs
+
+        await tf.nextFrame()
+      }
+
+    }
+  })
 }
 
 async function showPredictions(model) {
@@ -27,3 +63,6 @@ function createModel() {
   const model = createConvModel()
   return model
 }
+
+//TODO: main function, load data, train model, show what model predict on unseen data.
+ui.setTrainButtonCallBack(() => {})
